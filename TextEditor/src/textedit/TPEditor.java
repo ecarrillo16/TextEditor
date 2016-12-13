b7b33801c63818d05bc24ab0577a524a3211dbe8
Edgar Carrillo <eldemente16.4@gmail.com>
2016-12-13 16:43:30 -0500

    Se crea el archivo TPEditor con los metodos necesarios, estos se irán modificando en el transcurso del proyecto.

--- TextEditor/src/textedit/TPEditor.java
+++ TextEditor/src/textedit/TPEditor.java
@@ -1,5 +1,159 @@
 package textedit;
 
-public class TPEditor {
-
+import java.awt.BorderLayout;    //importamos todo lo que se utilizará
+import java.awt.Component;
+import java.awt.Container;
+import java.awt.Dimension;
+import java.awt.Insets;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.io.File;
+import javax.swing.BorderFactory;
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JCheckBoxMenuItem;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JPopupMenu;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JToolBar;
+import javax.swing.KeyStroke;
+import javax.swing.UIManager;
+import javax.swing.WindowConstants;
+import javax.swing.event.CaretEvent;
+import javax.swing.event.CaretListener;
+import javax.swing.event.UndoableEditEvent;
+import javax.swing.event.UndoableEditListener;
+import javax.swing.text.BadLocationException;
+import javax.swing.undo.UndoManager;
+ 
+public class TPEditor {    //clase publica TPEditor
+ 
+    private JFrame jFrame;            //instancia de JFrame (ventana principal)
+    private JMenuBar jMenuBar;        //instancia de JMenuBar (barra de menú)
+    private JToolBar jToolBar;        //instancia de JToolBar (barra de herramientas)
+    private JTextArea jTextArea;      //instancia de JTextArea (area de edición)
+    private JPopupMenu jPopupMenu;    //instancia de JPopupMenu (menú emergente)
+    private JPanel statusBar;         //instancia de JPanel (barra de estado)
+ 
+    private JCheckBoxMenuItem itemLineWrap;         //instancias de algunos items de menú que necesitan ser accesibles
+    private JCheckBoxMenuItem itemShowToolBar;
+    private JCheckBoxMenuItem itemFixedToolBar;
+    private JCheckBoxMenuItem itemShowStatusBar;
+    private JMenuItem mbItemUndo;
+    private JMenuItem mbItemRedo;
+    private JMenuItem mpItemUndo;
+    private JMenuItem mpItemRedo;
+ 
+    private JButton buttonUndo;    //instancias de algunos botones que necesitan ser accesibles
+    private JButton buttonRedo;
+ 
+    private JLabel sbFilePath;    //etiqueta que muestra la ubicación del archivo actual
+    private JLabel sbFileSize;    //etiqueta que muestra el tamaño del archivo actual
+    private JLabel sbCaretPos;    //etiqueta que muestra la posición del cursor en el área de edición
+ 
+    private boolean hasChanged = false;    //el estado del documento actual, no modificado por defecto
+    private File currentFile = null;       //el archivo actual, ninguno por defecto
+ 
+    private final EventHandler eventHandler;          //instancia de EventHandler (la clase que maneja eventos)
+    private final ActionPerformer actionPerformer;    //instancia de ActionPerformer (la clase que ejecuta acciones)
+    private final UndoManager undoManager;            //instancia de UndoManager (administrador de edición)
+ 
+    public static void main(String[] args) {    //punto de entrada del programa
+    }
+ 
+    public TPEditor() {    //constructor de la clase TPEditor
+    }
+ 
+    private void buildMenuBar() {    //construye la barra de menú
+    }
+ 
+    private void buildToolBar() {    //construye la barra de herramientas
+    }
+ 
+    private void buildTextArea() {    //construye el área de edición
+    }
+ 
+    private void buildStatusBar() {    //construye la barra de estado
+    }
+ 
+    private void buildPopupMenu() {    //construye el menú emergente
+    }
+ 
+    private void showPopupMenu(MouseEvent e) {    //despliega el menú emergente
+    }
+ 
+    void updateControls() {    //actualiza algunos componentes de la GUI
+    }
+ 
+    EventHandler getEventHandler() {    //retorna la instancia de EventHandler (la clase que maneja eventos)
+    }
+ 
+    UndoManager getUndoManager() {    //retorna la instancia de UndoManager (administrador de edición)
+    }
+ 
+    boolean documentHasChanged() {    //retorna el estado de la variable hasChanged
+    }
+ 
+    void setDocumentChanged(boolean hasChanged) {    //establece el estado de la variable hasChanged
+    }
+ 
+    JTextArea getJTextArea() {    //retorna la instancia de JTextArea (área de edición)
+    }
+ 
+    JFrame getJFrame() {    //retorna la instancia de JFrame (ventana principal)
+    }
+ 
+    File getCurrentFile() {    //retorna el valor de la variable currentFile
+    }
+ 
+    void setCurrentFile(File currentFile) {    //establece el valor de la variable currentFile
+    }
+ 
+    JLabel getJLabelFilePath() {    //retorna la instancia de la etiqueta sbFilePath
+    }
+ 
+    JLabel getJLabelFileSize() {    //retorna la instancia de la etiqueta sbFileSize
+    }
+ 
+    /* la clase EventHandler extiende e implementa las clases e interfaces necesarias para
+    atender y manejar los eventos sobre la GUI principal del editor */
+    class EventHandler extends MouseAdapter implements ActionListener,
+                                                       CaretListener,
+                                                       UndoableEditListener {
+ 
+        @Override
+        public void actionPerformed(ActionEvent ae) {    //implemento de la interface ActionListener
+        }
+ 
+        @Override
+        public void caretUpdate(CaretEvent ce) {    //implemento de la interface CaretListener
+        }
+ 
+        @Override
+        public void undoableEditHappened(UndoableEditEvent uee) {    //implemento de la interface UndoableEditListener
+        }
+ 
+        @Override
+        public void mousePressed(MouseEvent me) {    //herencia de la clase MouseAdapter
+        }
+ 
+        @Override
+        public void mouseReleased(MouseEvent me) {    //herencia de la clase MouseAdapter
+        }
+    }
 }
